696262146953848f93698d3c1216c5fe01adef85
MeeadAlotaibi <Meead.Jazi.elq.js@tuwaiq.edu.sa>
2021-11-29 22:11:48 +0300

    commit 5

--- routers/controllers/role.js
+++ routers/controllers/role.js
@@ -1,18 +1,33 @@
 const roleModel = require("../../db/models/role");
 
-
 ///////////////////////////
 
-const create = (req,res) =>{
-    const {role , permossion} = req.body ;
-    const newRole = new roleModel({ role, permission});
-    newRole
-      .save()
-      .then((result) => {
-        res.json(result);
-      })
-      .catch((err) => {
-        res.status(400).json(err);
-      });
+const create = (req, res) => {
+  const { role, permossion } = req.body;
+  const newRole = new roleModel({ role, permission });
+  newRole
+    .save()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+////////////////////////////////////////
+const getRoles = (req, res) => {
+  roleModel
+    .find({})
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 };
 /////////////////////////////////////////
+
+module.exports = {
+  create,
+  getRoles,
+};
