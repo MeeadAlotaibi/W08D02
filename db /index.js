e9e1654882bac37eb9c96864c145a9060069ce6c
MeeadAlotaibi <Meead.Jazi.elq.js@tuwaiq.edu.sa>
2021-11-30 21:39:40 +0300

    modify the DataBase connection

--- db /index.js
+++ db /index.js
@@ -1,26 +1,15 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
-
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
-mongoose.connect(process.env.DB_URI, options).then(() => {
-  console.log("DB can be used now");
-});
-//////////////
-
-const mongoose = require("mongoose");
 require("dotenv").config();
+
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-mongoose.connect(process.env.DB, options).then(() => {
-  console.log("DB ready to use");
+mongoose.connect(process.env.DB, options, () => {
+  try {
+    console.log("DB READY TO USE");
+  } catch (error) {
+    console.error(error);
+  }
 });
-
-
